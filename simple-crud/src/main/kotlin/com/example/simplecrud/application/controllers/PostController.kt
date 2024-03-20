8b33381d882d20b1fff36d8d38f3558b0b518e98
hoonloper <yonghoonj98@gmail.com>
2024-03-20 20:52:45 +0900

    Refator: 폴더구조 변경

--- simple-crud/src/main/kotlin/com/example/simplecrud/application/controllers/PostController.kt
+++ simple-crud/src/main/kotlin/com/example/simplecrud/application/controllers/PostController.kt
@@ -1,4 +1,4 @@
-package com.example.simplecrud
+package com.example.simplecrud.application.controllers
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
