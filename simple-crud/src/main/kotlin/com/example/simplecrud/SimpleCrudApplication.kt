b683e15021a162e2a7e0a8bbbdc9ea16ebdb24d3
hoonloper <yonghoonj98@gmail.com>
2024-03-20 12:40:29 +0900

    Setting: kotlin spring setting

---
+++ simple-crud/src/main/kotlin/com/example/simplecrud/SimpleCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.simplecrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleCrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleCrudApplication>(*args)
+}
