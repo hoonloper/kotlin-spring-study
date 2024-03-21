065f204df3b640fcd51d41062b836015ac6bb027
hoonloper <yonghoonj98@gmail.com>
2024-03-21 19:01:27 +0900

    Setting: kotest 의존성 설정

--- simple-crud/build.gradle.kts
+++ simple-crud/build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
