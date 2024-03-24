43ba1fed1a7d5e7fd324c603aae073e4532c1ac3
hoonloper <yonghoonj98@gmail.com>
2024-03-24 11:48:50 +0900

    Setting: 인증, 인가 학습을 위한 프로젝트 세팅

---
+++ auth/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	val kotlinVersion = "1.8.22"
+	id("org.springframework.boot") version "3.1.10"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+	kotlin("plugin.jpa") version kotlinVersion
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+allOpen {
+	// plugin.spring에서 오픈해주는 것들 외에 추가적으로 오픈할 것들
+	annotation("jakarta.persistence.Entity")
+}
+
+noArg {
+	// 매개변수가 없는 생성자를 자동으로 추가해주는 것을 명시
+	annotation("jakarta.persistence.Entity")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
