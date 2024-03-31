4001c31c05d275f37eb3d6671e3026e43ff3b979
hoonloper <yonghoonj98@gmail.com>
2024-03-31 16:19:39 +0900

    Feat: redisson locking 적용

--- simple-redis/src/main/kotlin/com/example/simpleredis/service/TicketService.kt
+++ simple-redis/src/main/kotlin/com/example/simpleredis/service/TicketService.kt
@@ -5,15 +5,19 @@ import com.example.simpleredis.entity.ApplyEntity
 import com.example.simpleredis.entity.TicketEntity
 import com.example.simpleredis.repository.ApplyJpaRepository
 import com.example.simpleredis.repository.TicketJpaRepository
+import org.redisson.api.RLock
+import org.redisson.api.RedissonClient
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.lang.Exception
+import java.util.concurrent.TimeUnit
 
 @Service
 class TicketService(
     private val ticketJpaRepository: TicketJpaRepository,
-    private val applyJpaRepository: ApplyJpaRepository
+    private val applyJpaRepository: ApplyJpaRepository,
+    private val redissonClient: RedissonClient
 ) {
     fun getTickets(): List<TicketEntity> {
         return ticketJpaRepository.findAll()
@@ -29,6 +33,13 @@ class TicketService(
 
     @Transactional
     fun applyTicket(userId: Long, ticketId: Long) {
+        val rLock = redissonClient.getLock(ticketId.toString()) // Lock 호출
+
+        val locked = rLock.tryLock(5, 1, TimeUnit.SECONDS) // Lock 획득 시도
+        if (!locked) {
+            throw Exception("Lock 획득 실패")
+        }
+
         if (applyJpaRepository.existsByUserIdAndTicketId(userId, ticketId)) {
             throw Exception("이미 예매된 티켓")
         }
@@ -38,9 +49,19 @@ class TicketService(
             throw Exception("품절됨")
         }
 
-        ticket.increaseCount()
+        try {
+            ticket.increaseCount()
+            ticketJpaRepository.save(ticket)
 
-        ticketJpaRepository.save(ticket)
-        applyJpaRepository.save(ApplyEntity(userId = userId, ticketId = ticketId))
+            applyJpaRepository.save(ApplyEntity(userId = userId, ticketId = ticketId))
+        } catch (e: Exception) {
+            throw Exception(e.message)
+        } finally {
+            if (rLock.isHeldByCurrentThread) {
+                rLock.unlock()
+            } else {
+                throw Exception("Lock 소유중이 아닐 때")
+            }
+        }
     }
 }
