3810d535350bd5a5fb7f3f9ec985147205ad20b9
hoonloper <yonghoonj98@gmail.com>
2024-03-31 14:24:56 +0900

    Feat: 상태코드 적용

--- simple-redis/src/main/kotlin/com/example/simpleredis/controller/TicketController.kt
+++ simple-redis/src/main/kotlin/com/example/simpleredis/controller/TicketController.kt
@@ -5,10 +5,12 @@ import com.example.simpleredis.dto.TicketDto
 import com.example.simpleredis.entity.ApplyEntity
 import com.example.simpleredis.entity.TicketEntity
 import com.example.simpleredis.service.TicketService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,15 +22,20 @@ class TicketController(
     fun getTickets(): List<TicketEntity> {
         return ticketService.getTickets()
     }
+
     @GetMapping("/applied")
     fun getAppliedTickets(): List<ApplyEntity> {
         return ticketService.getAppliedTickets()
     }
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun addTicket(@RequestBody ticketDto: TicketDto) {
         ticketService.addTicket(ticketDto)
     }
+
     @PostMapping("/apply")
+    @ResponseStatus(HttpStatus.CREATED)
     fun applyTicket(@RequestBody ticketApplyDto: TicketApplyDto) {
         ticketService.applyTicket(ticketApplyDto.userId, ticketApplyDto.ticketId)
     }
