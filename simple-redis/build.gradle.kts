8e5cc32461acf9694ec7312870a5f4b8d62c393e
hoonloper <yonghoonj98@gmail.com>
2024-03-31 14:46:35 +0900

    Setting: 코루틴 의존성 설정

--- simple-redis/build.gradle.kts
+++ simple-redis/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
