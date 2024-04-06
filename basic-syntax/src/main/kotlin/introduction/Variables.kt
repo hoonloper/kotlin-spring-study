34536c258cf1d99fcfa7728405321cb081ab97b0
hoonloper <yonghoonj98@gmail.com>
2024-04-06 18:18:23 +0900

    Add: Variables 문법 추가

---
+++ basic-syntax/src/main/kotlin/introduction/Variables.kt
@@ -0,0 +1,19 @@
+package introduction
+
+fun main() {
+    var a: String = "initial"
+    println(a)
+    val b: Int = 1
+    val c= 3
+
+    var e: Int
+    println(e)
+
+    val d: Int
+
+    if (someCondition()) {
+        d = 1
+    } else {
+        d = 2
+    }
+}
