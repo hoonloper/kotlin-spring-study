138a83064bf6e3a2ea2a22da604e7625ab68bf99
hoonloper <yonghoonj98@gmail.com>
2024-04-06 19:28:12 +0900

    Add: inheritance 추가

---
+++ basic-syntax/src/main/kotlin/introduction/inheritance/ParameterizedConstructor.kt
@@ -0,0 +1,15 @@
+package introduction.inheritance
+
+open class Tiger(val origin:String) {
+    fun sayHello() {
+        println("A tiger from $origin says: grrhhh!")
+    }
+}
+
+class SiberianTiger: Tiger("Siberia")
+
+fun main() {
+    val tiger: Tiger = SiberianTiger()
+
+    tiger.sayHello()
+}
