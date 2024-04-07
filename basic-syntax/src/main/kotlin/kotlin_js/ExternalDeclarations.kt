6bb498dc402cb84f415a9b655d3c23e772cd23c9
hoonloper <yonghoonj98@gmail.com>
2024-04-07 13:46:32 +0900

    Add: Kotlin/JS 추가

---
+++ basic-syntax/src/main/kotlin/kotlin_js/ExternalDeclarations.kt
@@ -0,0 +1,7 @@
+package kotlin_js
+
+external fun alert(msg: String)
+
+fun main() {
+    alert("Hi!")
+}
---
+++ basic-syntax/src/main/kotlin/kotlin_js/HtmlBuilder.kt
@@ -0,0 +1,35 @@
+package kotlin_js
+
+val result = html {
+    head {
+        title { +"HTML encoding with Kotlin" }
+    }
+    body {
+        h1 { +"HTML encoding with Kotlin" }
+        p {
+            +"this format can be used as an"
+            +"alternative markup to HTML"
+        }
+
+        // an element with attributes and text content
+        a(href = "http://kotlinlang.org") { +"Kotlin" }
+
+        // mixed content
+        p {
+            +"This is some"
+            b { +"mixed" }
+            +"text. For more see the"
+            a(href = "http://kotlinlang.org") {
+                +"Kotlin"
+            }
+            +"project"
+        }
+        p {
+            +"some text"
+            ul {
+                for (i in 1..5)
+                    li { +"${i}*2 = ${i*2}" }
+            }
+        }
+    }
+}
