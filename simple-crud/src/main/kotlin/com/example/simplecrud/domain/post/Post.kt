666956eaffe23f65977cc93cca2990b31d54a14a
hoonloper <yonghoonj98@gmail.com>
2024-04-09 19:37:02 +0900

    Fix: Getter 제거

--- simple-crud/src/main/kotlin/com/example/simplecrud/domain/post/Post.kt
+++ simple-crud/src/main/kotlin/com/example/simplecrud/domain/post/Post.kt
@@ -3,19 +3,17 @@ package com.example.simplecrud.domain.post
 import com.example.simplecrud.domain.common.BaseEntity
 import com.example.simplecrud.domain.post.dto.PostUpdateRequestDto
 import jakarta.persistence.*
-import lombok.Getter
 import java.time.LocalDateTime
 
-@Getter
 @Entity
 @Table(name = "posts")
 class Post (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-    @Column
+    @Column(nullable = false)
     var title: String,
-    @Column
+    @Column(nullable = false)
     var description: String,
 ): BaseEntity() {
     fun update(postUpdateRequestDto: PostUpdateRequestDto) {
