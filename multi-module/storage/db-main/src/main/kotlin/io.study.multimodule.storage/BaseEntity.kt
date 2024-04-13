4f7c05e8eca1f6487b82ab21c497e43d47d29a48
hoonloper <yonghoonj98@gmail.com>
2024-04-13 22:41:22 +0900

    Chore: 마지막 라인 줄바꿈 추가

--- multi-module/storage/db-main/src/main/kotlin/io.study.multimodule.storage/BaseEntity.kt
+++ multi-module/storage/db-main/src/main/kotlin/io.study.multimodule.storage/BaseEntity.kt
@@ -6,7 +6,6 @@ import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.ZonedDateTime
 
-
 @MappedSuperclass
 internal abstract class BaseEntity {
     @Id
