f869c463c9573f59158a486d2003993bf70ac5f0
hoonloper <yonghoonj98@gmail.com>
2024-04-16 21:50:10 +0900

    Setting: simple-file 프로젝트 생성

---
+++ simple-file/src/test/kotlin/com/study/simplefile/SimpleFileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.simplefile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleFileApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
