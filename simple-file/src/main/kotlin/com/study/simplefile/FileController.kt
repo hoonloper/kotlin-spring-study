620dcd0cb1c76aca4f861f111622941711229739
hoonloper <yonghoonj98@gmail.com>
2024-04-17 20:48:54 +0900

    Feat: 이미지 파일 저장 로직 구현

--- simple-file/src/main/kotlin/com/study/simplefile/FileController.kt
+++ simple-file/src/main/kotlin/com/study/simplefile/FileController.kt
@@ -1,16 +1,32 @@
 package com.study.simplefile
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
 
 @RestController
 @RequestMapping("/simple/file")
 class FileController(
     private val fileService: FileService
 ) {
-    @PostMapping
-    fun save() {
-        fileService.save()
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun save(
+        @RequestParam("img") img: MultipartFile
+    ): ResponseEntity<String> {
+        return try {
+            val imgPath = fileService.save(img)
+
+            ResponseEntity.ok().body(imgPath)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image")
+        }
     }
 }
--- simple-file/src/main/kotlin/com/study/simplefile/FileService.kt
+++ simple-file/src/main/kotlin/com/study/simplefile/FileService.kt
@@ -1,20 +1,46 @@
 package com.study.simplefile
 
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
 import java.time.LocalDateTime
+
+const val UPLOAD_ROOT_PATH = "uploads"
 
 @Service
 class FileService(
     private val fileRepository: FileRepository
 ) {
-    fun save() {
+    fun save(img: MultipartFile): String {
+        // 파일 이름을 임의로 생성합니다. 중복을 피하기 위해 UUID를 사용할 수도 있습니다.
+        val imgName = getImgName(img)
+
+        val directory = getDirectory()
+        val imgPath = File(directory.absolutePath + File.separator + imgName)
+
+        img.transferTo(imgPath)
+
         val fileEntity = FileEntity(
-            name = "",
-            path = "",
-            size = 0L,
+            name = imgName,
+            path = imgPath.path,
+            size = img.size,
             createdDate = LocalDateTime.now()
         )
 
         fileRepository.save(fileEntity)
+
+        return imgPath.path
     }
+
+    private fun getDirectory(): File {
+        val directory = File(UPLOAD_ROOT_PATH)
+
+        if (!directory.exists()) {
+            directory.mkdir()
+        }
+
+        return directory
+    }
+
+    private fun getImgName(img: MultipartFile) = "${System.currentTimeMillis()}_${img.originalFilename}"
 }
