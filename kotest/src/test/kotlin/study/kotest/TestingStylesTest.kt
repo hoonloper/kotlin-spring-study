3871e3dcd472166a84854d2a443e2f96ac250ecc
hoonloper <yonghoonj98@gmail.com>
2024-05-23 22:17:50 +0900

    Test: AnnotationSpec 작성

--- kotest/src/test/kotlin/study/kotest/TestingStylesTest.kt
+++ kotest/src/test/kotlin/study/kotest/TestingStylesTest.kt
@@ -139,3 +139,20 @@ class ExpectSpecTest : ExpectSpec({
         // ...
     }
 })
+
+class AnnotationSpecTest : AnnotationSpec() {
+    @BeforeEach
+    fun beforeTest() {
+        println("Before each test")
+    }
+
+    @Test
+    fun test1() {
+        1 shouldBe 1
+    }
+
+    @Test
+    fun test2() {
+        3 shouldBe 3
+    }
+}
