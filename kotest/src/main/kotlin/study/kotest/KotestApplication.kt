afa7c3494912cdcfb05678afb597c9eff2cbb69b
hoonloper <yonghoonj98@gmail.com>
2024-05-23 20:59:18 +0900

    Setting: kotest study 프로젝트 생성

---
+++ kotest/src/main/kotlin/study/kotest/KotestApplication.kt
@@ -0,0 +1,11 @@
+package study.kotest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotestApplication>(*args)
+}
---
+++ kotest/src/test/kotlin/study/kotest/KotestApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
