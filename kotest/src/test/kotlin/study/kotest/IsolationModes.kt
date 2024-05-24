88af5849b6308d549f629fc1cc15c2ea29d2d21f
hoonloper <yonghoonj98@gmail.com>
2024-05-24 21:48:03 +0900

    Test: InstancePerLeaf

--- kotest/src/test/kotlin/study/kotest/IsolationModes.kt
+++ kotest/src/test/kotlin/study/kotest/IsolationModes.kt
@@ -95,3 +95,37 @@ class SingleInstanceTest2 : WordSpec() {
         }
     }
 }
+
+class InstancePerLeafTest : WordSpec() {
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
+
+    init {
+        "a" should {
+            println("Hello")
+            "b" {
+                println("From")
+            }
+            "c" {
+                println("Sam")
+            }
+        }
+    }
+}
+
+class InstancePerLeafTest2 : WordSpec() {
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
+
+    val counter = AtomicInteger(0)
+
+    init {
+        "a" should {
+            println("a=" + counter.getAndIncrement())
+            "b" {
+                println("b=" + counter.getAndIncrement())
+            }
+            "c" {
+                println("c=" + counter.getAndIncrement())
+            }
+        }
+    }
+}
