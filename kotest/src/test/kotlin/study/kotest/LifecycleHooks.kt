b68dd11a9d1af19bea2a7ff0cbf84b2cbe29590c
hoonloper <yonghoonj98@gmail.com>
2024-05-25 21:03:29 +0900

    Test: LifecycleHooks DSL methods

--- kotest/src/test/kotlin/study/kotest/LifecycleHooks.kt
+++ kotest/src/test/kotlin/study/kotest/LifecycleHooks.kt
@@ -1,5 +1,7 @@
 package study.kotest
 
+import io.kotest.assertions.fail
+import io.kotest.core.spec.BeforeTest
 import io.kotest.core.spec.style.WordSpec
 
 class LifecycleHooks
@@ -14,6 +16,32 @@ class LifecycleHooksTest : WordSpec({
     "테스트" should {
         "실행" {
             println("테스트 실행!")
+        }
+    }
+})
+
+val startTest: BeforeTest = {
+    println("before test $it")
+}
+
+class FirstTest : WordSpec({
+    // startTest 콜백으로 사용
+    beforeTest(startTest)
+
+    "테스트" should {
+        "실행" {
+            println("!!!!")
+        }
+    }
+})
+
+class SecondTest : WordSpec({
+    // startTest 콜백으로 사용
+    beforeTest(startTest)
+
+    "테스트" should {
+        "실패" {
+            fail("펑!")
         }
     }
 })
