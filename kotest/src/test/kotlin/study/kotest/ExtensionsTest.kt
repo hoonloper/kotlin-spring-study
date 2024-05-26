32dfaf9623d33b2e483f90747115a24e76ddcbad
hoonloper <yonghoonj98@gmail.com>
2024-05-26 11:42:53 +0900

    Test: ExtensionTest

---
+++ kotest/src/test/kotlin/study/kotest/ExtensionsTest.kt
@@ -0,0 +1,26 @@
+package study.kotest
+
+import io.kotest.core.listeners.AfterSpecListener
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.WordSpec
+
+class ExtensionsTest
+
+class MyTestListener : BeforeSpecListener, AfterSpecListener {
+    override suspend fun beforeSpec(spec: Spec) {
+        println("kafka 연동")
+    }
+
+    override suspend fun afterSpec(spec: Spec) {
+        println("kafka 연동 해제")
+    }
+}
+
+class ExtensionTest : WordSpec({
+    extension(MyTestListener())
+
+    "비즈니스 로직 실행" `when` {
+        println("실행 완료!")
+    }
+})
