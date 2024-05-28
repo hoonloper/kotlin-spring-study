ae72e511682159f26973abe27ac066979e72c0d9
hoonloper <yonghoonj98@gmail.com>
2024-05-28 23:00:10 +0900

    Test: NestedDataTest

--- kotest/build.gradle.kts
+++ kotest/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
     testImplementation("io.kotest:kotest-assertions-core:5.9.0")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
-    testImplementation("io.kotest:kotest-framework-datatest")
+//    testImplementation("io.kotest:kotest-framework-datatest")
     // mockk
     testImplementation("io.mockk:mockk:1.13.8")
 }
