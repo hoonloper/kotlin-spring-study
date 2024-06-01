07cbe1c1aafd568fb4b5c45cf46e043458c7e729
hoonloper <yonghoonj98@gmail.com>
2024-06-01 23:19:17 +0900

    Remove

--- sample/src/main/java/com/bank/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.bank;
-
-public class BadRequestException extends RuntimeException {
-  public BadRequestException() {
-    super("Invalid input size, you must input 3 lengtrh");
-  }
-}
