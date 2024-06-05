03eb51be2c38af024e2d3ec2a4a9cd5e58effb96
hoonloper <yonghoonj98@gmail.com>
2024-06-05 21:12:42 +0900

    Generic: 배열과 리스트, 제네릭과 무공변

--- advanced-kotlin/.idea/sonarlint/issuestore/index.pb
+++ advanced-kotlin/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,16 @@
 
 G
 src/main/kotlin/Main.kt,d/8/d83ae3847b7d408136086cc5e9e95f5f7c126ed5
+O
+src/main/kotlin/generic/Cage.kt,f/e/feb509f1dc1c96e203a9b37df513081c7a539cb7
+Q
+!src/main/kotlin/generic/Animal.kt,5/0/50f1e41b12d9b724b61cf3077c96d7efdf84d6cf
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+P
+ src/main/kotlin/generic/Cage2.kt,8/d/8d2785bb1de926e04e5be056c8df75cae63dd0b9
--- advanced-kotlin/src/main/kotlin/generic/Cage.kt
+++ advanced-kotlin/src/main/kotlin/generic/Cage.kt
@@ -25,6 +25,21 @@ fun main() {
 
     val fishCage = Cage2<Fish>()
     // fishCage.moveFrom(goldFishCage) // 왜 안될까?
+    // *** 타입 파라미터에 넣은 클래스가 상속 관계에 있더라도 제네릭 클래스로 들어가면 상속 관계는 무시한다. ***
+    // 이것이 무공변 (in-variant, 불공변)이라 한다.
+
+    // Java의 배열은 하위 타입이 상위 타입이 될 수 있다.
+    // String[] -> Object[] - 이것은 공변(co-variant)이라 한다.
+    /**
+     * String[] strs = new String[]{"A", "B", "C"};
+     * Object[] objs = strs;
+     * objs[0] = 1; // java.lang.ArrayStoreException: java.lang.Integer
+     * 공변하기 때문에 다른 타입을 넣을 수 있다.
+     * 그러나 이것은 런타임 환경에서 잡히기 때문에 매우 위험하다. (컴파일에서 잡히는 게 좋다)
+     *
+     * List<String> strs = List.of("A", "B", "C")
+     * List<Object> objs = strs // 에러발생!! List 제네릭은 무공변하게 만들었다!
+     */
 }
 
 class Cage {
@@ -44,18 +59,18 @@ class Cage {
 }
 
 // 제네릭
-class Cage2<T> {
-    private val animals: MutableList<T> = mutableListOf()
-
-    fun getFirst(): T {
-        return animals.first()
-    }
-
-    fun put(animal: T) {
-        this.animals.add(animal)
-    }
-
-    fun moveFrom(cage: Cage2<T>) {
-        this.animals.addAll(cage.animals)
-    }
-}
+//class Cage2<T> {
+//    private val animals: MutableList<T> = mutableListOf()
+//
+//    fun getFirst(): T {
+//        return animals.first()
+//    }
+//
+//    fun put(animal: T) {
+//        this.animals.add(animal)
+//    }
+//
+//    fun moveFrom(cage: Cage2<T>) {
+//        this.animals.addAll(cage.animals)
+//    }
+//}
