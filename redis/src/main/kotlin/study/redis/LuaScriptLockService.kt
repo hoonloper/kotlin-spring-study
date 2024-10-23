e1a5f30589f1928d27979b9bc1552c7cbfde88ab
정용훈 <yonghoonj98@gmail.com>
2024-10-23 20:18:11 +0900

    Feat: LuaScript 분산락 코드 및 테스트 작성

---
+++ redis/src/main/kotlin/study/redis/LuaScriptLockService.kt
@@ -0,0 +1,54 @@
+package study.redis
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.script.DefaultRedisScript
+import org.springframework.stereotype.Service
+import java.time.Duration
+
+@Service
+class LuaScriptLockService(
+    private val redisTemplate: RedisTemplate<String, Any>,
+) : LockService {
+    companion object {
+        private const val DEFAULT_KEY = "normal-lock"
+        private const val DEFAULT_VALUE = "locking"
+        private val DEFAULT_TTL = Duration.ofSeconds(3)
+
+        private val ACQUIRE_LOCK_SCRIPT =
+            """ 
+            if redis.call("SET", KEYS[1], ARGV[1], "NX", "EX", ARGV[2]) then
+                return 1
+            else
+                return 0
+            end
+            """.trimIndent()
+        private val RELEASE_LOCK_SCRIPT =
+            """
+            if redis.call("GET", KEYS[1]) == ARGV[1] then
+                return redis.call("DEL", KEYS[1])
+            else
+                return 0
+            end
+            """.trimIndent()
+    }
+
+    override fun lock(
+        key: String?,
+        ttl: Duration?,
+    ): Boolean {
+        return redisTemplate.execute(
+            DefaultRedisScript(ACQUIRE_LOCK_SCRIPT, Boolean::class.java),
+            listOf(key ?: DEFAULT_KEY),
+            DEFAULT_VALUE,
+            (ttl ?: DEFAULT_TTL).toMillis().toString(),
+        )
+    }
+
+    override fun unlock(key: String?): Boolean {
+        return redisTemplate.execute(
+            DefaultRedisScript(RELEASE_LOCK_SCRIPT, Boolean::class.java),
+            listOf(key ?: DEFAULT_KEY),
+            DEFAULT_VALUE,
+        )
+    }
+}
--- redis/src/test/kotlin/study/redis/LockServiceTest.kt
+++ redis/src/test/kotlin/study/redis/LockServiceTest.kt
@@ -6,6 +6,7 @@ import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
@@ -15,35 +16,46 @@ import java.util.concurrent.atomic.AtomicInteger
 @SpringBootTest
 class LockServiceTest(
     @Autowired
-    private val normalLockService: LockService,
+    private val redisTemplate: RedisTemplate<String, Any>,
 ) : DescribeSpec({
+        lateinit var lockService: LockService
+
         describe("NormalLockService") {
+            lockService = NormalLockService(redisTemplate)
+
+            val defaultKey = "normal"
+            var key = ""
+
+            afterEach {
+                lockService.unlock(key = key)
+            }
+
             context("잠금되어 있지 않으면") {
-                val key = "normal-lock"
+                key = "$defaultKey-lock"
 
                 it("잠금에 성공한다") {
-                    val locked = normalLockService.lock(key = key, ttl = Duration.ofMillis(10))
+                    val locked = lockService.lock(key = key, ttl = Duration.ofMillis(200))
 
                     locked.shouldBeTrue()
                 }
             }
 
             context("잠금되어 있으면") {
-                val key = "already-locked"
+                key = "$defaultKey-already-locked"
 
                 beforeEach {
-                    normalLockService.lock(key = key, ttl = Duration.ofMillis(10))
+                    lockService.lock(key = key, ttl = Duration.ofMillis(200))
                 }
 
                 it("잠금에 실패한다") {
-                    val locked = normalLockService.lock(key = key, ttl = Duration.ofMillis(10))
+                    val locked = lockService.lock(key = key, ttl = Duration.ofMillis(200))
 
                     locked.shouldBeFalse()
                 }
             }
 
             context("동시에 잠금을 획득하려 하면") {
-                val key = "duplicate-call"
+                key = "$defaultKey-duplicate-call"
 
                 val threadCount = 10
                 val readyLatch = CountDownLatch(threadCount)
@@ -61,12 +73,91 @@ class LockServiceTest(
                                 startLatch.await()
 
                                 val locked =
-                                    normalLockService.lock(key = key, ttl = Duration.ofMillis(100))
+                                    lockService.lock(key = key, ttl = Duration.ofMillis(200))
                                 if (locked) {
                                     lockSuccessCounter.incrementAndGet()
                                 }
                             } finally {
-                                println("Done $it")
+                                val unlocked = lockService.unlock(key = key)
+                                println("Done $it - unlocked: $unlocked")
+                            }
+                        }
+                    }
+
+                    readyLatch.await()
+
+                    startLatch.countDown()
+
+                    executor.shutdown()
+                    executor.awaitTermination(1, TimeUnit.SECONDS)
+
+                    lockSuccessCounter.get().shouldBe(1)
+                }
+            }
+        }
+
+        describe("LuaScriptLocService") {
+            lockService = LuaScriptLockService(redisTemplate)
+
+            val defaultKey = "lua-script"
+            var key = ""
+
+            afterEach {
+                lockService.unlock(key = key)
+            }
+
+            context("잠금되어 있지 않으면") {
+                key = "$defaultKey-lock"
+
+                it("잠금에 성공한다") {
+                    val locked = lockService.lock(key = key, ttl = Duration.ofMillis(200))
+
+                    locked.shouldBeTrue()
+                }
+            }
+
+            context("잠금되어 있으면") {
+                key = "$defaultKey-already-locked"
+
+                beforeEach {
+                    lockService.lock(key = key, ttl = Duration.ofMillis(200))
+                }
+
+                it("잠금에 실패한다") {
+                    val locked = lockService.lock(key = key, ttl = Duration.ofMillis(200))
+
+                    locked.shouldBeFalse()
+                }
+            }
+
+            context("동시에 잠금을 획득하려 하면") {
+                key = "$defaultKey-duplicate-call"
+
+                val threadCount = 10
+                val readyLatch = CountDownLatch(threadCount)
+                val startLatch = CountDownLatch(1)
+                val executor = Executors.newFixedThreadPool(threadCount)
+
+                it("성공한다") {
+                    val lockSuccessCounter = AtomicInteger(0)
+
+                    repeat(threadCount) {
+                        executor.submit {
+                            try {
+                                readyLatch.countDown()
+                                readyLatch.await()
+                                startLatch.await()
+
+                                val locked =
+                                    lockService.lock(key = key, ttl = Duration.ofMillis(200))
+                                if (locked) {
+                                    lockSuccessCounter.incrementAndGet()
+                                }
+                            } catch (e: Exception) {
+                                println(e)
+                            } finally {
+                                val unlocked = lockService.unlock(key = key)
+                                println("Done $it - unlocked: $unlocked")
                             }
                         }
                     }
